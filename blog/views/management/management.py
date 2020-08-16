6d887d126d1a830e51a9f35779870b7c27bf7532
wzrgit <>
2020-08-16 17:05:05 +0800

    modified dashboard

--- blog/views/management/management.py
+++ blog/views/management/management.py
@@ -6,8 +6,10 @@ from blog.views import common
 
 def dashboard(request):
     # TODO check auth
+    recent_posts = Posts.objects.values('id', 'public_time', 'title', 'subtitle').order_by('-public_time')[0:5]
     counts = {'posts': Posts.objects.count(),
               'albums': Album.objects.count()}
     content = {'common': common.Common.get_commons(request),
-               'counts': counts}
+               'counts': counts,
+               'recent_posts': recent_posts}
     return render(request, 'management/dashboard.html', content)
