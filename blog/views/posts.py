b5d9f0185a2b7fcbd2757a0d0db66c4b33154d29
wzrgit <>
2020-08-24 23:02:36 +0800

    edit posts

--- blog/views/posts.py
+++ blog/views/posts.py
@@ -5,20 +5,35 @@ from .common import Common
 
 def post_list(request, curr_page=0, category=-1):
     posts_per_page = 5
+    text_length = 300
 
     from_num = int(curr_page) * posts_per_page
     to_num = from_num + posts_per_page
     if request.user.is_authenticated:
-        posts = Posts.objects.values('id', 'title', 'subtitle', 'public_time', 'visit_status', 'cover').exclude(
-            visit_status=VisitStatus.Draft).order_by('-public_time')[from_num: to_num]
+        posts = Posts.objects.values('id', 'title', 'subtitle', 'public_time', 'visit_status', 'cover',
+                                     'content').exclude(visit_status=VisitStatus.Draft).order_by('-public_time')[
+                from_num: to_num]
     else:
-        posts = Posts.objects.values('id', 'title', 'subtitle', 'public_time', 'visit_status', 'cover').filter(
+        posts = Posts.objects.values('id', 'title', 'subtitle', 'public_time', 'visit_status', 'cover',
+                                     'content').filter(
             visit_status__in=(VisitStatus.Public, VisitStatus.Protected)).order_by('-public_time')[from_num: to_num]
 
-    categories = PostCategory.objects.all()
+    for p in posts:
+        if request.user.is_authenticated:
+            if len(p['content']) > 300:
+                p['content'] = p['content'][:300]
+        else:
+            if p['visit_status'] == VisitStatus.Public:
+                if len(p['content']) > 300:
+                    p['content'] = p['content'][:300]
+            else:
+                p['content'] = ''
+
+    categories = PostCategory.objects.all().values()
 
     content = {'common': Common.get_commons(request),
-               'posts': posts}
+               'posts': posts,
+               'categories':categories}
 
     theme = Common.get_commons(request)['theme']
     assert len(theme) > 0
